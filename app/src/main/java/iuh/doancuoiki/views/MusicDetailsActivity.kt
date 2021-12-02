20b9eed24ee0f03f3520cb9a781a6e0d133863d6
huuminh365@gmail.com <huuminh365@gmail.com>
2021-12-02 16:21:53 +0700

    second commit

--- app/src/main/java/iuh/doancuoiki/views/MusicDetailsActivity.kt
+++ app/src/main/java/iuh/doancuoiki/views/MusicDetailsActivity.kt
@@ -11,7 +11,7 @@ class MusicDetailsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.song_information)
+        setContentView(R.layout.lyrics)
 
         var bundle = intent.extras
 
